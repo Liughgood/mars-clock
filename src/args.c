8bf833812ca9f1fa28856e9b85e29d01f8e8e918
Genhao Liu <genhao.liu@thoughtworks.com>
2025-01-02 00:49:11 +0800

    feat: add help option and support long option

--- src/args.c
+++ src/args.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 #include "../include/args.h"
 #include "../include/clock.h"
 
@@ -8,9 +9,14 @@ void print_help() {
     printf("Usage: mars-clock [options]\n");
     printf("Options:\n");
     printf("  -h, --help    Display this help message\n");
+    printf("  -v, --version Display the version number\n");
     printf("\n");
     printf("If no options are provided, the Airy Mean Time (AMT) will be displayed.\n");
     printf("For more knowleage, you can get it from https://www.giss.nasa.gov/tools/mars24/help/notes.html in English, and from https://liughgood.github.io/GenhaoLog/nasa/mars/time/2024/12/14/note-Mars24-sunclock.html in Chinese.\n");
+}
+
+void print_version() {
+    printf("mars-clock version 1.0.0\n");
 }
 
 int handle_args(int argc, char *argv[]) {
@@ -19,11 +25,21 @@ int handle_args(int argc, char *argv[]) {
         return 0;
     }
 
+    struct option long_options[] = {
+        {"help", no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'v'},
+        {0, 0, 0, 0}
+    };
+
     int opt;
-    while ((opt = getopt(argc, argv, "h")) != -1) {
+    int option_index = 0;
+    while ((opt = getopt_long(argc, argv, "hv", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'h':
                 print_help();
+                return 0;
+            case 'v':
+                print_version();
                 return 0;
             default:
                 print_help();
